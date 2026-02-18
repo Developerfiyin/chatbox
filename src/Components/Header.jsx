2bade87968ab8f3973d30f776e36f8741c94bc4f
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-19 00:04:56 +0100

    Updated Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { FaRocketchat } from "react-icons/fa";
-
+import { useState } from 'react';
 const Header = () => {
+
+    const [expanded, setExpanded] = useState(false)
   return (
-    <main className='flex h-screen overflow-hidden bg-indigo-'>
-         <aside className='bg-red-500 flex h-screen '>
+    <main className='flex h-screen overflow-hidden bg-indigo-100 '>
+         <aside className='bg-purple-500 flex h-screen '>
+
+            <div></div>
             <FaRocketchat />
 
          </aside>
