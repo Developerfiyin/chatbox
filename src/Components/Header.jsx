41e2eb56931d235e524c69e58da3e5fb52a6c9c3
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-19 00:14:20 +0100

    Added a div with the button and text element in it.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
+import React from "react";
 import { FaRocketchat } from "react-icons/fa";
-import { useState } from 'react';
+import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
+
+import { useState } from "react";
 const Header = () => {
-
-    const [expanded, setExpanded] = useState(false)
+  const [expanded, setExpanded] = useState(false);
   return (
-    <main className='flex h-screen overflow-hidden bg-indigo-100 '>
-         <aside className='bg-purple-500 flex h-screen '>
-
-            <div></div>
-            <FaRocketchat />
-
-         </aside>
-
-
+    <main className="flex h-screen overflow-hidden bg-indigo-100 ">
+      <aside className="bg-purple-500 flex h-screen ">
+        <div>
+          <button>
+            <IoChatbubbleEllipsesOutline />
+          </button>
+        </div>
+        <FaRocketchat />
+      </aside>
     </main>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
