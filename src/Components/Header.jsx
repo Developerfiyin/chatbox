ce84c74da3bea0484e590fcc58865ee4f67e0b5e
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-19 00:44:51 +0100

    Used react usestate to toggle between the icons

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { FaRocketchat } from "react-icons/fa";
 import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
+import { FiSidebar } from "react-icons/fi";
 
 import { useState } from "react";
 const Header = () => {
   const [expanded, setExpanded] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <main className="flex h-screen overflow-hidden bg-indigo-100 ">
       <aside className="bg-purple-500 flex h-screen ">
-        <div>
-          <button>
-            <IoChatbubbleEllipsesOutline />
+        <div onClick={() => setExpanded(!expanded)}>
+          <button onClick={() => setIsOpen(!isOpen)} className="h-11 w-11 ">
+            {isOpen ? <IoChatbubbleEllipsesOutline size={30} /> : <FiSidebar size={30}/>}
           </button>
         </div>
+
         <FaRocketchat />
       </aside>
     </main>
