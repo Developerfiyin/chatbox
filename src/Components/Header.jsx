777fffde8dc59f3e222487bdca0fdf88e64e3081
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-19 12:30:25 +0100

    Added function NavItem

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { FaRocketchat } from "react-icons/fa";
 import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
+import { LuNotebookPen } from "react-icons/lu";
+
 import { FiSidebar } from "react-icons/fi";
 import "./index.css";
 import { useState } from "react";
@@ -29,14 +31,21 @@ const Header = () => {
         </div>
 
         <nav className="flex-1 ">
-          <NavItem 
-          icon = { }
-          
-          />
+          <NavItem icon={<LuNotebookPen />} />
         </nav>
       </aside>
     </main>
   );
 };
+
+function NavItem({ icon, label, active, expanded }) {
+  return (
+    <button
+      className={`w-full flex items-center p-3.5 rounded-2xl transition-all ${active ? "bg-indigo-100 text-purple-100 " : "text-purple-200: bg-indigo-50"} `}
+    >
+      <span className={`text-2xl ${!expanded && "mx-auto"}`}>{icon}</span>
+    </button>
+  );
+}
 
 export default Header;
