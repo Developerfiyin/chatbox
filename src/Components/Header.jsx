72a033abcd4c00e03c566859d5c12e9ea65e41ab
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-19 11:29:56 +0100

    Updated button style

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -13,20 +13,27 @@ const Header = () => {
       <aside
         className={`bg-purple-300 flex flex-col  border-indigo-400 border-l px-6  ${expanded ? "w-96" : "w-20"} `}
       >
-        <div onClick={() => setExpanded(!expanded)} className="flex items-center h-20 ">
+        <div
+          onClick={() => setExpanded(!expanded)}
+          className="flex items-center h-20 "
+        >
           <button className="icon-container ">
             <IoChatbubbleEllipsesOutline size={30} className="icon-default" />
             <FiSidebar size={30} className="icon-hover" />
           </button>
-          <button className={`ml-auto transition-opacity ${expanded ? "opacity-80" : "opacity-0 hidden" }`} >
-            <FiSidebar size={30}/>
+          <button
+            className={`ml-auto transition-opacity ${expanded ? "opacity-80" : "opacity-0 hidden"}`}
+          >
+            <FiSidebar size={30} />
           </button>
         </div>
 
-        <nav>
-
+        <nav className="flex-1 ">
+          <NavItem 
+          icon = { }
+          
+          />
         </nav>
-        
       </aside>
     </main>
   );
