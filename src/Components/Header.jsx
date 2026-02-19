478eddad91a8d6cd83746ddf22b7303478c1d3fd
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-19 01:46:04 +0100

    Updated header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,24 +2,31 @@ import React from "react";
 import { FaRocketchat } from "react-icons/fa";
 import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
 import { FiSidebar } from "react-icons/fi";
-import './index.css'
+import "./index.css";
 import { useState } from "react";
-
 
 const Header = () => {
   const [expanded, setExpanded] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   return (
     <main className="flex h-screen overflow-hidden bg-indigo-100 ">
-      <aside className="bg-purple-500 flex h-screen ">
-        <div onClick={() => setExpanded(!expanded)}>
-          <button className="icon-container ">
-             <IoChatbubbleEllipsesOutline size={30} className="icon-default"/> 
-              <FiSidebar size={30} className="icon-hover"/>
+      <aside
+        className={`bg-purple-500 flex h-screen  ${expanded ? "w-96" : "w-24"} `}
+      >
+        <div onClick={() => setExpanded(!expanded)} className=" ">
+          <button className="icon-contianer ">
+            <IoChatbubbleEllipsesOutline size={30} className="icon-default" />
+            <FiSidebar
+              size={30}
+              className="icon-hover"
+              aria-label="Close sidebar" />
           </button>
-          <span> Chat Gpt</span>
+          <span> 
+            <button> 
+                < FiSidebar/>
+            </button>
+          </span>
         </div>
-
       </aside>
     </main>
   );
