4880c692bb595ed66929261acfcc76ec462c2fd0
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-19 14:02:04 +0100

    Updated Navbar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -13,33 +13,30 @@ const Header = () => {
   return (
     <main className="flex h-screen overflow-hidden bg-indigo-100 ">
       <aside
-        className={`bg-purple-300 flex flex-col  border-indigo-400 border-l px-6  ${expanded ? "w-96" : "w-20"} `}
+        className={`bg-purple-300 flex flex-col  border-indigo-400 border-l text-center ${expanded ? "w-96" : "w-13"} `}
       >
         <div
           onClick={() => setExpanded(!expanded)}
-          className="flex items-center h-20 "
+          className="h-20 flex items-center text-center "
         >
-          <button className="icon-container ">
-            <IoChatbubbleEllipsesOutline  className="icon-default" />
-            <FiSidebar size={30} className="icon-hover" />
+          <button className="icon-container px-4">
+            <IoChatbubbleEllipsesOutline className="icon-default text-2xl " />
+            <FiSidebar className="icon-hover text-2xl" />
           </button>
           <button
             className={`ml-auto transition-opacity ${expanded ? "opacity-90" : "opacity-0 hidden"}`}
           >
-            <FiSidebar size={30} />
+            <FiSidebar className="text-2xl" />
           </button>
         </div>
 
         <nav className="flex-1 px-3 space-y-2 mt-4">
-          <NavItem 
-          icon={<LuNotebookPen />}
-          label="New Chat"
-          active 
-          expanded={expanded}
+          <NavItem
+            icon={<LuNotebookPen />}
+            label="New Chat"
+            active
+            expanded={expanded}
           />
-
-
-
         </nav>
       </aside>
     </main>
@@ -49,13 +46,17 @@ const Header = () => {
 function NavItem({ icon, label, active, expanded }) {
   return (
     <button
-      className={`w-full flex items-center p-3.5 rounded-2xl transition-all ${active ? "text-purple-100 " : "text-purple-200 bg-indigo-50"}`}
+      className={`w-full flex items-center rounded-2xl transition-all ${active ? "text-purple-100 " : "text-purple-200 bg-indigo-50"}`}
     >
       <span className={`text-2xl ${!expanded && "mx-auto"}`}>{icon}</span>
 
-      <span className={`ml-4 overflow-hidden text-nowrap transition-all ${expanded ? "w-auto opacity-90" : "w-0 opacity-0"}`}>{label}</span>
+      <span
+        className={`ml-4 overflow-hidden text-nowrap transition-all ${expanded ? "w-auto opacity-90" : "w-0 opacity-0"}`}
+      >
+        {label}
+      </span>
     </button>
-  )
+  );
 }
 
 export default Header;
